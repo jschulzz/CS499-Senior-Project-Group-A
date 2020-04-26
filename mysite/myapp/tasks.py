5aa22ad569ed7e2705e6b08571dbc1e3c0d8050c
John Schulz <jr.john.schulz@gmail.com>
2020-04-26 18:45:43 -0500

    Update Botometer Url
    Botometer is deprecating their old api soon, so I updated to their newest URL

--- mysite/myapp/tasks.py
+++ mysite/myapp/tasks.py
@@ -36,7 +36,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 bom = botometer.Botometer(
-    wait_on_ratelimit=True, rapidapi_key=rapidapi_key, **twitter_app_auth
+    wait_on_ratelimit=True, rapidapi_key=rapidapi_key, **twitter_app_auth, botometer_api_url="https://botometer-pro.p.rapidapi.com"
 )
 
 # initial twitter search criteria
