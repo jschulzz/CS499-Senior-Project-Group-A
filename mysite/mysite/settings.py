e13dc2e384fcdbb7ad851dd40f9403f52e8ffe0b
John Schulz <jr.john.schulz@gmail.com>
2020-04-27 17:18:37 -0500

    Updated Searching
    Utilizes Django Queries more to make searching faster

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's01#t)uu3#x&$@!96t=#x$#p3tnmqr7_79t*c@vlet5z2n!j(w'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -79,11 +79,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'SupremeCourtTwitter',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
         'OPTIONS': {'charset': 'utf8mb4'},
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
         }
 }
 
@@ -91,9 +91,9 @@ LOGIN_REDIRECT_URL = '/scotustwitter'
 LOGOUT_REDIRECT_URL = '/scotustwitter'
 
 EMAIL_USE_TLS = True
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-#EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_PORT = 587
 
 # Password validation
